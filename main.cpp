bd4cdd95c5726e4b8c9cbbb1dea52a4743953996
funkycoder123 <109564658+funkycoder123@users.noreply.github.com>
2024-11-06 15:35:52 -0800

    changed the input numbers.to return a smaller output

--- main.cpp
+++ main.cpp
@@ -1,39 +1,29 @@
+// main.cpp
 /*
 Inserting the randomly generated integers into a BST generally gives a height in the order of:
 
-Your answer: Average-case height of a BST is O(log n).......
-
+answer: Average-case height of a BST is O(log n)
 */
 
-#include <cstdlib> // For rand() and srand()....
-#include <ctime>   // For time()...
-#include <chrono>
-#include <cmath>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
 #include <random>
 #include "BST.h"
 
-double log2(double d)
-{
-    return log(d) / log(2); // log() use e as base..
-}
-
 void insertRandomIntegers(BST<int> *root, int numIntegers)
 {
-    // The range of random integers
-    const int minValue = -1000000000;
-    const int maxValue = 1000000000;
+    const int minValue = -1000;
+    const int maxValue = 1000;
     int randomInteger;
 
-    // Generate a uniform distribution to generate random integers
-    random_device dev;
-    mt19937 rng(dev());
-    uniform_int_distribution<std::mt19937::result_type> dist(minValue, maxValue);
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(minValue, maxValue);
 
-    // Generate random integers (without duplicates) from the specified range..
     for (int i = 0; i < numIntegers; ++i)
     {
         randomInteger = dist(rng);
-        while (root->contains(randomInteger) == true)
+        while (root->contains(randomInteger))
         {
             randomInteger = dist(rng);
         }
@@ -45,21 +35,21 @@ int main()
 {
     BST<int> bst;
 
-    // Insert 100 random integers
-    insertRandomIntegers(&bst, 100);
+    // Insert a limited number of random integers
+    insertRandomIntegers(&bst, 20);
 
-    // Print BST contents
-    cout << "In-order Traversal:" << endl;
+    // Required output
+    std::cout << "In-order Traversal:" << std::endl;
     bst.printInOrder();
 
-    cout << "Level-wise Traversal:" << endl;
+    std::cout << "\nLevel-wise Traversal:" << std::endl;
     bst.printLevels();
 
-    cout << "Max Path:" << endl;
+    std::cout << "\nMax Path:" << std::endl;
     bst.printMaxPath();
 
-    cout << "Tree Size: " << bst.treeSize() << endl;
-    cout << "Tree Height: " << bst.treeHeight() << endl;
+    std::cout << "Tree Size: " << bst.treeSize() << std::endl;
+    std::cout << "Tree Height: " << bst.treeHeight() << std::endl;
 
     return 0;
 }
